3dd906078df7ab02c61bc3f9f39d01a2d2d873bd
lamproteam <lamproteamlt@gmail.com>
2021-01-21 11:43:59 +0200

    Revert "Edit check time add more"
    This reverts commit 917e64be588f8fef146848b749dcd6be67a0aac2.

--- check_time.py
+++ check_time.py
@@ -1,17 +1,12 @@
 from datetime import datetime
-import os
 
 def check_time_min():
 	""" Returns minute for users"""
 	timestamp = datetime.now()
 	hour = timestamp.hour
 	minute = timestamp.minute
-	second = timestamp.second
-	hm = (hour,minute,second)
+	hm = (hour,minute)
 	return hm
 
 def sey_hello(username):
 	return "Hello {}".format(username)
-
-def shutdown():
-	os.system("shutdown -p")	
