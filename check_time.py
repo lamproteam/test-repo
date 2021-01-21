6ea3c5e0951b0bfd5c3eef8268117eb43588ef41
lamproteam <lamproteamlt@gmail.com>
2021-01-21 11:47:29 +0200

    Edit file and add new shutdown function

--- check_time.py
+++ check_time.py
@@ -1,12 +1,17 @@
 from datetime import datetime
+import os
 
 def check_time_min():
 	""" Returns minute for users"""
 	timestamp = datetime.now()
 	hour = timestamp.hour
 	minute = timestamp.minute
-	hm = (hour,minute)
+	second = timestamp.second
+	hm = (hour,minute,second)
 	return hm
 
 def sey_hello(username):
 	return "Hello {}".format(username)
+
+def shutdown():
+	os.system("shutdown -p")
